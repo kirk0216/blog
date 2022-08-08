707690e547aa6e89e8e1f5a5c09726fe0efa7d22
Patrick Kirk <kirk0216@algonquinlive.com>
2022-08-08 12:51:11 -0400

    Remove unused import.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request, url_for, Markup)
+from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
