94b73bcce1db2d2105e0988150ffc567a086d126
Patrick Kirk <kirk0216@algonquinlive.com>
2022-08-10 15:51:19 -0400

    Added ability for a user to delete their own comments.

--- flaskr/comment.py
+++ flaskr/comment.py
@@ -8,7 +8,7 @@ bp = Blueprint('comment', __name__, url_prefix='/comment')
 
 def get_comments(post_id: int):
     comments = get_db().execute(
-        'SELECT c.body, c.created, u.username '
+        'SELECT c.id, c.author_id, c.body, c.created, u.username '
         'FROM comment c '
         'JOIN user u on c.author_id = u.id '
         'WHERE c.post_id = ?;',
@@ -34,5 +34,15 @@ def create(post_id: int):
 
         db.execute('INSERT INTO comment (post_id, author_id, body) VALUES (?, ?, ?);', (post_id, g.user['id'], body))
         db.commit()
+
+    return redirect(url_for('blog.view', id=post_id))
+
+
+@bp.route('/<int:post_id>/delete/<int:comment_id>')
+@login_required
+def delete(post_id: int, comment_id: int):
+    db = get_db()
+    db.execute('DELETE FROM comment WHERE id = ?;', (comment_id,))
+    db.commit()
 
     return redirect(url_for('blog.view', id=post_id))
