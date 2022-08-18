dfb1c0ff924d88a4e214f4fc998357f94ffb1c02
Patrick Kirk <kirk0216@algonquinlive.com>
2022-08-18 14:26:45 -0400

    Update comment tests to check JSON returned by comment functions.

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -2,16 +2,20 @@ from flaskr.db import get_db
 
 
 def test_comment_form_requires_login(client, auth):
+    required_elements = [
+        b'div id="comment-form"',
+        b'textarea id="body"',
+        b'button id="submit-comment"'
+    ]
+
     response = client.get('/1/')
-    assert b'action="/comment/1/create' not in response.data
-    assert b'textarea id="body"' not in response.data
-    assert b'input type="submit" value="Save"' not in response.data
+    for element in required_elements:
+        assert element not in response.data
 
     auth.login()
     response = client.get('/1/')
-    assert b'action="/comment/1/create' in response.data
-    assert b'textarea name="body"' in response.data
-    assert b'input type="submit" value="Save"' in response.data
+    for element in required_elements:
+        assert element in response.data
 
 
 def test_comment_validation(client, auth):
@@ -23,7 +27,11 @@ def test_comment_validation(client, auth):
 def test_comment(client, auth, app):
     auth.login()
     response = client.post('/comment/1/create', data={'body': 'created comment'})
-    assert response.headers['Location'] == '/1/'
+    assert response.is_json
+    assert 'success' in response.json
+    assert response.json['success']
+    assert 'html' in response.json
+    assert 'created comment' in response.json['html']
 
     with app.app_context():
         db = get_db()
@@ -34,20 +42,20 @@ def test_comment(client, auth, app):
 def test_delete_validation(client, auth, app):
     with app.app_context():
         db = get_db()
-        db.execute('UPDATE comment SET author_id = 5 WHERE id = 1;')
+        db.execute('UPDATE comment SET author_id = 2 WHERE id = 1;')
         db.commit()
 
     auth.login()
     # Error message is show if comment doesn't exist
-    response = client.post('/comment/1/delete/5', follow_redirects=True)
-    assert b'Comment with id 5 does not exist.' in response.data
+    response = client.post('/comment/delete/5', follow_redirects=True)
+    assert 'Comment with id 5 does not exist.' in response.json['error']
 
     # User doesn't see delete link for comments that aren't theirs
-    assert b'form action="/comment/1/delete/1' not in client.get('/1/').data
+    assert b'onclick="deleteComment(1);"' not in client.get('/1/').data
 
     # Error message is shown if user does not have permission to delete the comment
-    response = client.post('/comment/1/delete/1', follow_redirects=True)
-    assert b'You do not have permission to delete this comment.' in response.data
+    response = client.post('/comment/delete/1')
+    assert 'You do not have permission to delete this comment.' in response.json['error']
 
     # Confirm that the comment was not deleted.
     with app.app_context():
@@ -58,8 +66,10 @@ def test_delete_validation(client, auth, app):
 
 def test_delete(client, auth, app):
     auth.login()
-    response = client.post('/comment/1/delete/1')
-    assert response.headers['Location'] == '/1/'
+    response = client.post('/comment/delete/1')
+    assert response.is_json
+    assert 'success' in response.json
+    assert response.json['success']
 
     with app.app_context():
         db = get_db()
