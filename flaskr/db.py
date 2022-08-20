e2caf5f1f639884e5a20174fecd5259281e8900b
Patrick Kirk <kirk0216@algonquinlive.com>
2022-08-20 02:24:05 -0400

    Start implementing database objects. This will allow for SQLite to be used in development and PostGRES in production.

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,41 @@ import click
 from flask import current_app, g
 
 
+class Database:
+    connection = None
+    IntegrityError = sqlite3.IntegrityError
+
+    def get_connection(self):
+        raise NotImplementedError()
+
+    def execute(self, sql, parameters=()):
+        if self.get_connection() is not None:
+            return self.get_connection().execute(sql, parameters)
+
+    def executescript(self, script: bytes | str):
+        if self.get_connection() is not None:
+            return self.get_connection().executescript(script)
+
+    def commit(self):
+        if self.get_connection() is not None:
+            self.get_connection().commit()
+
+    def close(self):
+        if self.get_connection() is not None:
+            self.get_connection().close()
+
+
+class DevelopmentDatabase(Database):
+    def get_connection(self):
+        if self.connection is None:
+            connection_string = os.path.join(current_app.instance_path, current_app.config['DATABASE'])
+
+            self.connection = sqlite3.connect(connection_string, detect_types=sqlite3.PARSE_DECLTYPES)
+            self.connection.row_factory = sqlite3.Row
+
+        return self.connection
+
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
@@ -24,16 +59,7 @@ def init_db_command():
 
 def get_db():
     if 'db' not in g:
-        db_path = current_app.config['DATABASE']
-
-        if current_app.config['DATABASE'] != ':memory:':
-            db_path = os.path.join(current_app.instance_path, db_path)
-
-        print(db_path)
-
-        g.db = sqlite3.connect(db_path, detect_types=sqlite3.PARSE_DECLTYPES)
-
-        g.db.row_factory = sqlite3.Row
+        g.db = DevelopmentDatabase()
 
     return g.db
 
