73678c9d26b2cc3ee49a817a2d28c721d2177892
Patrick Kirk <kirk0216@algonquinlive.com>
2022-08-21 14:55:04 -0400

    Post double quotes around user in queries as user is a built-in table in postgres.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -14,7 +14,7 @@ def get_post(post_id: int, check_author: bool = True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username '
         'FROM post p '
-        'JOIN user u ON p.author_id = u.id WHERE p.id = ?;',
+        'JOIN "user" u ON p.author_id = u.id WHERE p.id = ?;',
         (post_id,)
     ).fetchone()
 
