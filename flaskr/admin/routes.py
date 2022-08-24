79e72b7e78f3f39098709952c1d8788c5f87bc67
Patrick Kirk <kirk0216@algonquinlive.com>
2022-08-24 15:03:17 -0400

    Add post management to admin panel.

--- flaskr/admin/routes.py
+++ flaskr/admin/routes.py
@@ -5,6 +5,7 @@ from sqlalchemy import text
 from . import bp
 from flaskr.auth import login_required, csrf_protection
 from flaskr.db import get_db
+from flaskr import utils
 
 sections = ['users', 'posts', 'comments']
 
@@ -59,3 +60,58 @@ def edit_user(id: int):
             return redirect(url_for('admin.list_users'))
 
     return render_template('admin/user_edit.html', user=user)
+
+
+@bp.route('/posts')
+@login_required
+def list_posts():
+    with get_db().connect() as conn:
+        posts = conn.execute(
+            text(
+                'SELECT p.id, p.author_id, p.created, p.title, p.body, u.username as "author" FROM post p '
+                'JOIN user u ON u.id = p.author_id;'
+            )
+        )
+
+        return render_template('admin/post_list.html', posts=posts)
+
+
+@bp.route('/posts/edit/<int:id>', methods=('GET', 'POST'))
+@login_required
+@csrf_protection
+def edit_post(id: int):
+    with get_db().connect() as conn:
+        post = conn.execute(
+            text(
+                'SELECT p.id, p.author_id, p.created, p.title, p.body, u.username as "author" FROM post p '
+                'JOIN user u ON u.id = p.author_id '
+                'WHERE p.id = :id;'
+            ),
+            {'id': id}
+        ).one_or_none()
+
+        if request.method == 'POST':
+            title = utils.get_form_value('title')
+            body = utils.get_form_value('body')
+            error = None
+
+            print('%s' % title)
+
+            if None in [title, body]:
+                error = 'Title and body cannot be empty.'
+            else:
+                try:
+                    conn.execute(
+                        text('UPDATE post SET title=:title, body=:body WHERE id=:id;'),
+                        {'title': title, 'body': body, 'id': id}
+                    )
+                    conn.commit()
+                except sqlalchemy.exc.DatabaseError:
+                    error = 'There was an error saving your changes.'
+
+            if error is not None:
+                flash(error)
+            else:
+                return redirect(url_for('admin.list_posts'))
+
+    return render_template('admin/post_edit.html', post=post)
