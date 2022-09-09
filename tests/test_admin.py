be99e4831bdfa6bb04ad1364fb7795cb31b00afc
Patrick Kirk <kirk0216@algonquinlive.com>
2022-09-09 19:00:43 -0400

    Finished tests for admin panel.

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,5 +1,8 @@
 import pytest
 
+from sqlalchemy import text
+
+from flaskr.db import get_db
 
 @pytest.mark.parametrize('page', (
     '/admin/',
@@ -14,21 +17,85 @@ def test_admin_authorization_required(client, auth, page):
 
 
 @pytest.mark.parametrize(('page', 'expected'), (
-        ('/admin/users', b''),
-        ('/admin/posts', b''),
-        ('/admin/comments', b'')
+        ('/admin/users', [b'<a href="/admin/users/edit/1">test</a>']),
+        ('/admin/posts', [b'<a href="/admin/posts/edit/1">test title</a>', b'<td>test</td>', b'2022-01-01 00:00:00']),
+        ('/admin/comments', [b'<a href="/admin/comments/edit/1">test title</a>', b'test', b'2022-02-01 00:00:00'])
 ))
 def test_lists(client, auth, page, expected):
-    pass
+    auth.login()
+    response = client.get(page)
+
+    assert response.status_code == 200
+
+    for part in expected:
+        assert part in response.data
 
 
-def test_edit_user(client, auth):
-    pass
+def test_edit_user(client, auth, app):
+    auth.login()
+    assert client.get('/admin/users/edit/1').status_code == 200
+
+    client.post('/admin/users/edit/1', data={'username': 'updated'})
+
+    with app.app_context():
+        with get_db().connect() as conn:
+            user = conn.execute(text('SELECT * FROM user WHERE id = 1;')).one()
+            assert user['username'] == 'updated'
 
 
-def test_edit_post(client, auth):
-    pass
+@pytest.mark.parametrize(('username', 'expected'), (
+    ('', b'Username cannot be blank.'),
+    ('other', b'Username is already in use.')
+))
+def test_edit_user_validate(client, auth, username, expected):
+    auth.login()
+
+    response = client.post('/admin/users/edit/1', data={'username': username})
+    assert expected in response.data
 
 
-def test_edit_comment(client, auth):
-    pass
+def test_edit_post(client, auth, app):
+    auth.login()
+    assert client.get('/admin/posts/edit/1').status_code == 200
+
+    client.post('/admin/posts/edit/1', data={'title': 'updated title', 'body': 'updated body'})
+
+    with app.app_context():
+        with get_db().connect() as conn:
+            post = conn.execute(text('SELECT * FROM post WHERE id = 1;')).one()
+            assert post['title'] == 'updated title'
+            assert post['body'] == 'updated body'
+
+
+@pytest.mark.parametrize(('title', 'body', 'expected'), (
+    ('', '', b'Title and body cannot be empty.'),
+    ('', 'body', b'Title and body cannot be empty.'),
+    ('title', '', b'Title and body cannot be empty.')
+))
+def test_edit_post_validate(client, auth, title, body, expected):
+    auth.login()
+
+    response = client.post('/admin/posts/edit/1', data={'title': title, 'body': body})
+    assert expected in response.data
+
+
+def test_edit_comment(client, auth, app):
+    auth.login()
+    assert client.get('/admin/comments/edit/1').status_code == 200
+
+    client.post('/admin/comments/edit/1', data={'body': 'updated body'})
+
+    with app.app_context():
+        with get_db().connect() as conn:
+            comment = conn.execute(text('SELECT * FROM comment WHERE id = 1;')).one()
+            assert comment['body'] == 'updated body'
+
+
+@pytest.mark.parametrize(('body', 'expected'), (
+    ('', b'Comment body cannot be empty.'),
+))
+def test_edit_comment_validate(client, auth, body, expected):
+    auth.login()
+
+    response = client.post('/admin/comments/edit/1', data={'body': body})
+    assert expected in response.data
